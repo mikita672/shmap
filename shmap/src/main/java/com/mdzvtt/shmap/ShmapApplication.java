d9665e21e6d2c6847b1d651ae3d74f4388be0264
Mikita Dzeviatau <9mikita@gmail.com>
2026-06-22 21:17:47 +0200

    chore: backend init

---
+++ shmap/src/main/java/com/mdzvtt/shmap/ShmapApplication.java
@@ -0,0 +1,13 @@
+package com.mdzvtt.shmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShmapApplication.class, args);
+	}
+
+}
---
+++ shmap/src/test/java/com/mdzvtt/shmap/ShmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdzvtt.shmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
