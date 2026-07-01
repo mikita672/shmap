2f72278774f9d4dc0b5b8affe92cc9d292e4d8e1
Mikita Dzeviatau <9mikita@gmail.com>
2026-07-01 22:52:18 +0200

    feat: add User entity implementation

---
+++ backend/src/main/java/com/mdzvtt/shmap/user/User.java
@@ -0,0 +1,24 @@
+package com.mdzvtt.shmap.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
+public class User {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
