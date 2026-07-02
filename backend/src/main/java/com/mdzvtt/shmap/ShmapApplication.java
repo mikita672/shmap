e728eb8fe4f936374e4b25fe9be818bf474fcbab
Mikita Dzeviatau <9mikita@gmail.com>
2026-07-02 17:17:21 +0200

    feat: introduce JwtService, update JwtAuthenticationFilter

--- backend/src/main/java/com/mdzvtt/shmap/ShmapApplication.java
+++ backend/src/main/java/com/mdzvtt/shmap/ShmapApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ShmapApplication {
-    public static void main(String[] args) {
+    void main(String[] args) {
         SpringApplication.run(ShmapApplication.class, args);
     }
 }
---
+++ backend/src/main/java/com/mdzvtt/shmap/configuration/JwtService.java
@@ -0,0 +1,37 @@
+package com.mdzvtt.shmap.configuration;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+    @Value("${application.security.jwt.secret-key}")
+    private String secretKey;
+
+    public String extractUserEmail(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser().verifyWith(getSigningKey()).build().parseSignedClaims(token).getPayload();
+    }
+
+    private SecretKey getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
