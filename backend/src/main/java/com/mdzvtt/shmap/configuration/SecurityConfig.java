0aef984c7af919eac6a0708c2810e7d8ced3e77a
Mikita Dzeviatau <9mikita@gmail.com>
2026-07-03 22:37:57 +0200

    feat: add auth endpoints to the filter excluded paths

--- backend/src/main/java/com/mdzvtt/shmap/configuration/SecurityConfig.java
+++ backend/src/main/java/com/mdzvtt/shmap/configuration/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("")
+                        .requestMatchers("/api/v1/auth/**")
                         .permitAll()
                         .anyRequest()
                         .authenticated()
