76c4fbfc2360c5d420c52e6e151c6817ec8dd954
Mikita Dzeviatau <9mikita@gmail.com>
2026-08-08 15:33:07 +0200

    feat: introduce login view; chore: remove autogenerated files

---
+++ frontend/app/(auth)/login.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+export default function LoginScreen() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-xl font-bold">Login Page</Text>
+    </View>
+  );
+}
