a2bd800e70390858665fa3ddbc407296fd1e9565
Mikita Dzeviatau <9mikita@gmail.com>
2026-08-08 15:19:31 +0200

    feat: add custom color palette

--- frontend/constants/theme.ts
+++ frontend/constants/theme.ts
@@ -5,25 +5,21 @@
 
 import { Platform } from 'react-native';
 
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
 
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
+    primary: "#A9B388",
+    secondary: "#FEFAE0",
+    textPrimary: "#FEFAE0",
+    textSecondary: "#B99470",
+    logo: "#C4661F"
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
+    primary: "#5F6F52",
+    secondary: "#B99470",
+    textPrimary: "#FEFAE0",
+    textSecondary: "#B99470",
+    logo: "#C4661F"
   },
 };
 
