d59ec0c914e0a6d8f3ba51fa293e3586a6b05621
Mikita Dzeviatau <9mikita@gmail.com>
2026-08-09 21:19:20 +0200

    feat: add axios and react-query dependencies, implement RegisterRequest and Authentication interfaces

---
+++ frontend/lib/types/api.ts
@@ -0,0 +1,17 @@
+export interface RegisterRequest {
+    firstname: string;
+    lastname: string;
+    username: string;
+    email: string;
+    password: string;
+}
+
+export interface AuthenticationRequest {
+    email: string;
+    password: string;
+}
+
+export interface AuthenticationResponse {
+    access_token: string;
+    refresh_token: string;
+}
