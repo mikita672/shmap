c98b146d5f7b886b6526d987832acc44c6c9d641
Mikita Dzeviatau <9mikita@gmail.com>
2026-08-09 21:00:58 +0200

    feat: add openapi dependency

--- backend/src/main/java/com/mdzvtt/shmap/configuration/SecurityConfig.java
+++ backend/src/main/java/com/mdzvtt/shmap/configuration/SecurityConfig.java
@@ -25,7 +25,10 @@ public class SecurityConfig {
                 http
                                 .csrf(csrf -> csrf.disable())
                                 .authorizeHttpRequests(auth -> auth
-                                                .requestMatchers("/api/v1/auth/**")
+                                                .requestMatchers("/api/v1/auth/**",
+                                                                "/swagger-ui/**",
+                                                                "/swagger-ui.html",
+                                                                "/v3/api-docs/**")
                                                 .permitAll()
                                                 .anyRequest()
                                                 .authenticated())
