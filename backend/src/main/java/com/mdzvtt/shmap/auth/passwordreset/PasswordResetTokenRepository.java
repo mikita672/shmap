9e24e44cbab2409232f6fd70c5479eb6b36363dd
Mikita Dzeviatau <9mikita@gmail.com>
2026-08-16 16:28:53 +0200

    feat: add PasswordResetTokenRepository implementation

---
+++ backend/src/main/java/com/mdzvtt/shmap/auth/passwordreset/PasswordResetTokenRepository.java
@@ -0,0 +1,12 @@
+package com.mdzvtt.shmap.auth.passwordreset;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.mdzvtt.shmap.user.User;
+
+public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
+    Optional<PasswordResetToken> findByUser(User user);
+
+    void deleteByUser(User user);
+}
