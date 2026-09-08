de8edc992102450dede28e587c7fc406d1a24288
Mikita Dzeviatau <9mikita@gmail.com>
2026-09-08 15:27:25 +0200

    feat: move logout button to the friends tab

--- frontend/app/(tabs)/friends.tsx
+++ frontend/app/(tabs)/friends.tsx
@@ -1,10 +1,21 @@
+import { useAuth } from "@/hooks/useAuth";
 import React from "react";
 import { View, Text } from "react-native";
+import { Button } from "@/components/ui/button";
 
 export default function FriendsScreen() {
+  const { signOut } = useAuth();
+
   return (
     <View className="flex-1 justify-center items-center bg-background">
       <Text className="text-3xl font-bold text-foreground">Friends</Text>
+      <Button
+        variant="destructive"
+        className="rounded-full h-[60px] px-12"
+        onPress={() => signOut()}
+      >
+        <Text className="text-white font-semibold text-lg">Log Out</Text>
+      </Button>
     </View>
   );
 }
