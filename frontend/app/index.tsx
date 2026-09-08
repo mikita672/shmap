1bb935c3af061e5e887622609e2fe54a0a7635b3
Mikita Dzeviatau <9mikita@gmail.com>
2026-09-08 13:29:33 +0200

    feat: udpate index.tsx

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { View, Text } from "react-native";
 import { Button } from "@/components/ui/button";
 import { useAuth } from "@/hooks/useAuth";
+import { Navbar } from "@/components/ui/navbar";
 
 export default function IndexScreen() {
   const { signOut } = useAuth();
 
   return (
     <View className="flex-1 justify-center items-center bg-background p-6">
+      <Navbar title="SHMAP" onLogout={signOut} />
       <Text className="text-3xl font-bold mb-8 text-foreground">
         Welcome to SHMAP!
       </Text>
