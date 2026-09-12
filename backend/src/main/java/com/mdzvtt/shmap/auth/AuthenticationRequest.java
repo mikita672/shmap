e8b1f9f6692d535df8f6aa2bf85076a2352d9cd5
Mikita Dzeviatau <9mikita@gmail.com>
2026-09-12 22:20:59 +0200

    feat: add email validation to AuthenticationRequest

--- backend/src/main/java/com/mdzvtt/shmap/auth/AuthenticationRequest.java
+++ backend/src/main/java/com/mdzvtt/shmap/auth/AuthenticationRequest.java
@@ -1,5 +1,6 @@
 package com.mdzvtt.shmap.auth;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,6 +13,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class AuthenticationRequest {
     @NotBlank(message = "Email is required")
+    @Email(message = "Please provide a valid email address")
     private String email;
 
     @NotBlank(message = "Password is required")
